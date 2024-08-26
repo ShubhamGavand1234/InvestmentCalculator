758454bf9b05c3a0acd1defb0160a9543fba85ff
ShubhamGavand1234 <shubhamgavand@yahoo.in>
2024-08-26 12:01:03 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import investmentLogog from "../../public/investment-calculator-logo.png";
+
+function Header() {
+  return (
+    <>
+      <header id="header">
+        <img src={investmentLogog} />
+        <h1>Investment Calculator</h1>
+      </header>
+    </>
+  );
+}
+
+export default Header;
